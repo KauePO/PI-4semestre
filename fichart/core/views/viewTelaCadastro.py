a794022f570bb87fa4e5c995cf531b057aa6fe35
jp8002 <162762832+jp8002@users.noreply.github.com>
2025-10-01 20:27:40 -0300

    ✨ FEAT
    Adição das funções de Login e Cadastro

---
+++ fichart/core/views/viewTelaCadastro.py
@@ -0,0 +1,23 @@
+from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+class viewTelaCadastro(View):
+    def get(self, request):
+        return render(request, "templateCadastro.html")
+
+    def post(self, request):
+        print(request.POST['username'])
+        form = UserCreationForm(request.POST)
+
+        if not form.is_valid():
+            print(form.errors)
+            return render(request, "templateCadastro.html")
+
+        form.save()
+        
+        return redirect('telaLogin')
+
+        
