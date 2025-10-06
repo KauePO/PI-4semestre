f9a4250ce89e567e738284ad2ce9a1ce2fed2d73
KauePO <kaueperes2004@gmail.com>
2025-10-06 17:17:00 -0300

    🔧 CHORE
    Atualiza o arquivo urls.py do projeto core, criando o url para a viewTelaAntecedente

--- fichart/core/urls.py
+++ fichart/core/urls.py
@@ -8,11 +8,13 @@ from .views.viewTelaEscolhaRaca import viewTelaEscolhaRaca
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views.viewTelaClasses import viewTelaClasses
 from .views.viewTelaMagia import viewTelaMagia
+from .views.viewTelaAntecedente import viewTelaAntecedente
 
 urlpatterns = [
     path('', viewTelaLogin.as_view(), name="telaLogin"),
     path('cadastro/', viewTelaCadastro.as_view(), name="telaCadastro"),
     path('selecaoRaca/',viewTelaEscolhaRaca.as_view(), name="TelaEscolhaRaca"),
     path('selecaoClasse/',viewTelaClasses.as_view(), name="TelaClasses"),
-    path("selecaoMagia/",viewTelaMagia.as_view(), name="TelaMagia")
+    path("selecaoMagia/",viewTelaMagia.as_view(), name="TelaMagia"),
+    path('selecaoAntecedente/',viewTelaAntecedente.as_view(), name="TelaAntecedente")
 ]
