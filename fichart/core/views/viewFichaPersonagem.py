126fc3d9a7983229dad4689ca325ca3f3668cb21
KauePO <kaueperes2004@gmail.com>
2025-10-23 15:50:40 -0300

    ✨ FEAT
    Adiciona a seção Truques e Magias, não totalmente estilizada mas funcional

--- fichart/core/views/viewFichaPersonagem.py
+++ fichart/core/views/viewFichaPersonagem.py
@@ -23,10 +23,20 @@ class viewFichaPersonagem(LoginRequiredMixin,View):
         if lista_idiomas:
             lista_decodificada = urllib.parse.unquote(lista_idiomas)
             idiomas = json.loads(lista_decodificada)
-        
+        #=========================================================================
+        lista_truques = request.COOKIES.get('truques')
+
+        if lista_truques:
+            lista_decodificada = urllib.parse.unquote(lista_truques)
+            truques = json.loads(lista_decodificada)
+        #=========================================================================
+        lista_magias = request.COOKIES.get('magias')
+        if lista_magias:
+            lista_decodificada = urllib.parse.unquote(lista_magias)
+            magias = json.loads(lista_decodificada)
 
 
         personagem = Personagem.objects.get(id_personagem=1)
 
         form = personagemForm()
-        return render(request, 'templateFichaPersonagem.html', {'personagem': personagem, 'form': form, 'proficiencias': proficiencias, 'idiomas': idiomas})
+        return render(request, 'templateFichaPersonagem.html', {'personagem': personagem, 'form': form, 'proficiencias': proficiencias, 'idiomas': idiomas, 'truques': truques, 'magias': magias})
