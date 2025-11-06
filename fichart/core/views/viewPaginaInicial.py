23aed7c11b078a0e240e3dc196d111dc4c357202
jp8002 <162762832+jp8002@users.noreply.github.com>
2025-11-06 00:10:20 -0300

    ✨Feat - Criação da página inicial
    Este commit adiciona o design da página inicial e implementa suas principais funcionalidades:
    
     - Logout: Implementa a função de logout do usuário.
    
     - Criação de ficha: Adiciona o card e a funcionalidade de criação de fichas.
    
     - Listagem de fichas: Implementa a exibição das fichas já criadas.
    
     - Limitação por plano: Restringe as funcionalidades da tela inicial conforme o plano ativo do usuário.

--- fichart/core/views/viewPaginaInicial.py
+++ fichart/core/views/viewPaginaInicial.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 from django.templatetags.static import static
 from ..forms import nomeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from core.models import Personagem, Usuario
 
 class viewPaginaInicial(LoginRequiredMixin, View):
     def get(self, request):
-        return render(request, "templatePaginaInicial.html")
+       
+        personagens = Personagem.objects.filter(usuario__user=request.user)
+        usuario = Usuario.objects.get(user=request.user)
+        
+        context = {
+            "personagens":personagens,
+            "QTD_fichas": personagens.count(),
+            "plano_ativo":usuario.plano_ativo
+            
+        }
+        return render(request, "templatePaginaInicial.html",context)
     
